f7c098fc5f89d0398cfbc836ef311d36a1a9c1ba
sun4ay <60418547+sun4ay@users.noreply.github.com>
2021-02-05 12:30:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/zxlhhyccc/luci-ssrplus.git package/luci-ssrplus
+#git clone https://github.com/zxlhhyccc/luci-ssrplus.git package/luci-ssrplus
+git clone https://github.com/fw876/helloworld.git package/helloworld
